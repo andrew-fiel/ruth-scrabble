27490102dda28a3efc3f721a3a49bf61f72d1297
Andrew Fiel <andrew.w.fiel@gmail.com>
2019-10-16 12:21:28 -0600

    Initial Commit

---
+++ firstAttempt.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    main()
+
+def generate_options(hand, boardState):
+#hand should be list of current tiles in hand
+#boardState should be two-dimensional array representing board

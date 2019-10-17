55b09f932a937e614243cca1193de5d6e4c33b5a
Andrew Fiel <andrew.w.fiel@gmail.com>
2019-10-16 18:29:30 -0600

    Attempting to load dictionary as data structure

--- firstAttempt.py
+++ firstAttempt.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     main()
 
-def generate_options(hand, boardState):
+def generate_options(rack, boardState):
 #hand should be list of current tiles in hand
 #boardState should be two-dimensional array representing board
